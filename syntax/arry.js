a1512116cc8bdb7706373926ca9b85642395672b
Jiyoung <thdwldud8@gmail.com>
2021-10-12 03:59:22 +0900

    2021-10-12-배열

---
+++ syntax/arry.js
@@ -0,0 +1,15 @@
+//배열
+
+var arr = ['A', 'B', 'C', 'D'];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+arr[2] = 3;
+console.log(arr); //배열은 0부터
+
+console.log(arr.length); //길이는 1부터
+
+arr.push('E');
+console.log(arr);
